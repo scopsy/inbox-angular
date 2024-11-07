9b6da102a1d06ee38679b3d1eef1d649c296942a
Dima Grossman <dima@grossman.io>
2024-11-07 11:54:36 +0200

    feat: 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NovuUI } from '@novu/js/ui';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('notificationInbox') notificationInbox!: ElementRef<HTMLElement>;
   title = 'inbox-angular';
+
+  ngAfterViewInit() {
+    const novu = new NovuUI({
+      options: {
+        applicationIdentifier: '123',
+        subscriberId: '456',
+      },
+    });
+
+    novu.mountComponent({
+      name: 'Inbox',
+      props: {},
+      element: this.notificationInbox.nativeElement,
+    });
+  }
 }
